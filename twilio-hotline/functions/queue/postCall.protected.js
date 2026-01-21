f85f9be292ccdf970ae04f4983b0b29be1bd6bd9
thandal <than@timbrel.org>
2026-01-21 07:04:24 -0500

    rename

--- twilio-hotline/functions/queue/postCall.protected.js
+++ twilio-hotline/functions/queue/postCall.protected.js
@@ -1,27 +1,33 @@
 exports.handler = async function (context, event, callback) {
   //console.log("POSTCALL " + event.taskSid + " " + event.language + " " + event.callerFrom);
-  const { sayLangMap } = require(Runtime.getAssets()['/language.js'].path);
-  const postMap = {
-    "es": "Llamada finalizada. " +
-      "Presione 1 para escuchar el número de nuevo. " +
-      "Presione 2 para bloquearlo. " +
-      "Presione 3 para marcarlo de nuevo.",
-    "en": "Call finished. " +
-      "Press 1 to repeat the caller's number. " +
-      "Press 2 to add the caller to the blocklist. " +
-      "Press 3 to call the caller back",
-  }
+  const { sayLangMap, messagesMap } = require(Runtime.getAssets()['/language.js'].path);
   const twiml = new Twilio.twiml.VoiceResponse();
   if (!event.Digits) {
+    const client = context.getTwilioClient();
+    const reservation = await client.taskrouter.v1
+        .workspaces(context.WORKSPACE_SID)
+        .tasks(event.taskSid)
+        .reservations(event.reservationSid)
+        .fetch();
+    console.log("postCall reservationStatus " + reservation.reservationStatus);
+    if (reservation.reservationStatus !== 'completed') {
+      twiml.say("Call reservation failed with status " + reservation.reservationStatus);
+    }
     const gather = twiml.gather({ numDigits: 1 });
-    sayLangMap(gather, event.language, postMap);
+    sayLangMap(
+      gather,
+      event.language,
+      messagesMap[event.language].operator.postcall.options,
+      event.callerFrom
+    );
   } else if (event.Digits == 1) {
     // Repeat the caller's number
-    const postMap_repeat = {
-      "es": "Esta llamada era de {number}",
-      "en": "This call was from {number}",
-    }
-    sayLangMap(twiml, event.language, postMap_repeat, event.callerFrom);
+    sayLangMap(
+      twiml,
+      event.language,
+      messagesMap[event.language].operator.postcall.callerNumber,
+      event.callerFrom
+    );
     twiml.redirect('');  // Redirects to the current URL
   } else if (event.Digits == 2) {
     // Add to blocklist
@@ -42,18 +48,20 @@ exports.handler = async function (context, event, callback) {
       .environments(context.ENVIRONMENT_SID)
       .variables(blockListSid)
       .update({ value: blockList });
-    const postMap_block = {
-      "es": "Se bloquearon llamadas de {number}",
-      "en": "Blocking calls from {number}",
-    }
-    sayLangMap(twiml, event.language, postMap_block, event.callerFrom);
+    sayLangMap(
+      twiml,
+      event.language,
+      messagesMap[event.language].operator.postcall.blocking,
+      event.callerFrom
+    );
   } else if (event.Digits == 3) {
     // Call the number back
-    const postMap_dial = {
-      "es": "Marcando {number}",
-      "en": "Calling {number}",
-    }
-    sayLangMap(twiml, event.language, postMap_dial, event.callerFrom);
+    sayLangMap(
+      twiml,
+      event.language,
+      messagesMap[event.language].operator.postcall.callingBack,
+      event.callerFrom
+    );
     twiml.dial(event.callerFrom);
   }
   return callback(null, twiml);
--- twilio-hotline/functions/queue/preCall.protected.js
+++ twilio-hotline/functions/queue/preCall.protected.js
@@ -1,42 +1,35 @@
 exports.handler = function (context, event, callback) {
   //console.log("PRECALL " + event.taskSid + " " + event.reservationSid + " " + event.language + " " + event.callerFrom);
-  const { sayLangMap } = require(Runtime.getAssets()['/language.js'].path);
-  const introMap = {
-    es: "Presione 0 para acceptar la llamada. " +
-        "Presione 1 para repetir el número. " +
-        "Presione 3 para rechazarla.",
-    en: "Press 0 to accept the call. " +
-        "Press 1 to repeat the caller's number. " +
-        "Press 3 to reject the call.",
-  }
+  const { sayLangMap, messagesMap } = require(Runtime.getAssets()['/language.js'].path);
+
   const twiml = new Twilio.twiml.VoiceResponse();
+
   if (!event.Digits) {
     const gather = twiml.gather({
-        numDigits: 1,
-        timeout: 15
-      });
-    sayLangMap(gather, event.language, introMap, event.callerFrom);
+      numDigits: 1,
+      timeout: 15
+    });
+    sayLangMap(gather, event.language, messagesMap[event.language].operator.precall.intro, event.callerFrom);
     // By default, if no gather response happens within the timeout, reject the reservation.
-    console.log("AUTO-REJECTING the reservation " + event.reservationSid);
-    twiml.say("Auto-rejecting the call.");
-    twiml.redirect("/queue/rejectReservation?taskSid=" + event.taskSid + "&reservationSid=" + event.reservationSid);
-    twiml.hangup();
-  } if (event.Digits == 0) {
-    // Note: this "accepts" the task.
-    twiml.dial({ hangupOnStar: true })
-      .queue({ reservationSid: event.reservationSid });
-    twiml.redirect("/queue/postCall?" +
-      "taskSid=" + event.taskSid +
-      "&callerFrom=" + encodeURIComponent(event.callerFrom) +
-      "&language=" + event.language);
-  } if (event.Digits == 3) {
-    console.log("REJECTING the reservation " + event.reservationSid);
-    twiml.say("Rejecting the call.");
+    sayLangMap(
+      twiml,
+      event.language,
+      messagesMap[event.language].operator.precall.noResponse,
+      event.callerFrom
+    );
     twiml.redirect("/queue/rejectReservation?taskSid=" + event.taskSid + "&reservationSid=" + event.reservationSid);
     twiml.hangup();
   } else {
-    // We say 'dial 1 to repeat', but really, if they dial anything except the cases above, we repeat.
-    twiml.redirect('');  // Redirects to the current URL
+    // Note: this "accepts" the task (see https://www.twilio.com/docs/taskrouter/lifecycle-task-state)
+    twiml.dial({ hangupOnStar: true })
+      .queue({ reservationSid: event.reservationSid });
+    // After the call, we pop back here.
+    twiml.redirect("/queue/postCall?" +
+      "taskSid=" + event.taskSid +
+      "reservationSid=" + event.reservationSid +
+      "&callerFrom=" + encodeURIComponent(event.callerFrom) +
+      "&language=" + event.language);
   }
+
   return callback(null, twiml);
 };
