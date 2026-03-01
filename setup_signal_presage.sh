03a045fd54ba9f72b1446fac25144ef31b8295b6
thandal <than@timbrel.org>
2026-03-01 18:59:14 -0500

    Working binary and improved script

--- setup_signal_presage.sh
+++ setup_signal_presage.sh
@@ -4,14 +4,39 @@ echo This script will help you set up a new Signal account using presage-cli.
 
 PRESAGE_BIN="twilio-hotline/assets/presage-cli.private.bin"
 PRESAGE_DB="/tmp/presage.db.enc"
+ENV_FILE="twilio-hotline/.env"
+
+# Utility for rewriting env variables
+set_env_var() {
+    local key=$1
+    local value=$2
+    local file=$3
+
+    # create the file if it doesn't exist
+    touch $file
+
+    # Escape value for use in sed (handling / in the value)
+    local escaped_value=$(echo "$value" | sed 's/\//\\\//g')
+
+    # If the key exists, replace the entire line
+    if grep -q "^${key}=" "$file"; then
+        sed -i.bak "s/^${key}=.*/${key}=${escaped_value}/" "$file"
+        rm "$file.bak"
+    # Otherwise, append the new key-value pair
+    else
+        echo "${key}=${value}" >> "$file"
+    fi
+}
 
 # HACK DEBUG
 #PRESAGE_PASSPHRASE=`uuidgen`
 PRESAGE_PASSPHRASE=a34e34f55-a10f-4b91-b666-f7ebd3d1b25c
 
-echo Your presage database passphrase is 
-echo $PRESAGE_PASSPHRASE
-echo "PRESAGE_PASSPHRASE=\"$PRESAGE_PASSPHRASE\"" >> twilio-hotline/.env
+echo
+echo PRESAGE_PASSPHRASE $PRESAGE_PASSPHRASE
+echo
+
+set_env_var "PRESAGE_PASSPHRASE" $PRESAGE_PASSPHRASE $ENV_FILE
 
 PRESAGE_PRE="$PRESAGE_BIN --sqlite-db-path $PRESAGE_DB --passphrase $PRESAGE_PASSPHRASE"
 
@@ -40,29 +65,32 @@ if [ "$SIGNAL_ACCOUNT" != "" ]; then
     echo "You already seem to have an account set up"
     echo $SIGNAL_ACCOUNT
     echo "If you want to set up again, delete $PRESAGE_DB"
+else
+    # Proceed with registration!
+    echo Go to https://signalcaptchas.org/registration/generate and follow the instructions to generate a signal captcha code.
+    echo Once you have solved the captcha, don\'t open Signal. Right click on the "Open Signal" link and copy the link.
+    read -p "Then paste the signal captcha code here and press enter:" SIGNAL_CAPTCHA
+    
+    echo "Go to twilio messages https://console.twilio.com/us1/monitor/logs/sms to find the confirmation code for the next step"
+    $PRESAGE_PRE register --servers production --phone-number $PHONE_NUMBER --captcha $SIGNAL_CAPTCHA
+    # Then have to monitor the SMS messages, and type in the confirmation code.
 fi
 
-# Proceed with registration!
-echo Go to https://signalcaptchas.org/registration/generate and follow the instructions to generate a signal captcha code.
-echo Once you have solved the captcha, don\'t open Signal. Right click on the "Open Signal" link and copy the link.
-read -p "Then paste the signal captcha code here and press enter:" SIGNAL_CAPTCHA
+echo Now invite this number -- $PHONE_NUMBER -- to your group, and then send a message, so that we have a message to receive. You can safely re-run this script.
 
-echo "Go to twilio messages https://console.twilio.com/us1/monitor/logs/sms to find the confirmation code for the next step"
-$PRESAGE_PRE register --servers production --phone-number $PHONE_NUMBER --captcha $SIGNAL_CAPTCHA
-# Then have to monitor the SMS messages, and type in the confirmation code.
-
-# TODO: Split apart the register and verify steps in presage-cli...
-
-
-# First have to receive message *from the group* before we get the group key.
-# You'll need to invite the account to the group and then send some message to the group.
-./presage-cli --sqlite-db-path $PRESAGE_DB --passphrase $PRESAGE_PASSPHRASE receive
+# Now have to receive message *from the group* before we get the group key.
+$PRESAGE_PRE sync --stop-after-empty-queue
 
 # Then we can list the groups
-./presage-cli --sqlite-db-path $PRESAGE_DB --passphrase $PRESAGE_PASSPHRASE list-groups
+GROUP_KEY=`$PRESAGE_PRE list-groups | awk '{ print $1 }'`
 
-# TODO: store the GROUP_KEY in twilio-hotline/.env
+echo
+echo GROUP_KEY $GROUP_KEY
+echo
+
+set_env_var "GROUP_KEY" $GROUP_KEY $ENV_FILE
+
 GROUP_KEY=b877c0fd2bf514c5ce5d9b49842a23405f6017242409d074260e4c062b6393aa
 
 # Now we can send our first message!
-./presage-cli --sqlite-db-path $PRESAGE_DB --passphrase $PRESAGE_PASSPHRASE send-to-group --master-key $GROUP_KEY --message "Test message 1"
+$PRESAGE_PRE send-to-group --master-key $GROUP_KEY --message "Test message"
