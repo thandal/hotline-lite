af76a6462753f6d27c11cb902c9d038745bc1cbe
thandal <than@timbrel.org>
2026-04-28 14:31:21 -0400

    remove hardcoded values

--- twilio_get_env.sh
+++ twilio_get_env.sh
@@ -1,10 +1,7 @@
 # Hotline-lite
-SERVICE_SID=ZS5845044ef796db5f7e53c3a7e34e43d8
-ENVIRONMENT_SID=ZE7382245b8083b8dc13669e3b4e3954b0
+SERVICE_SID=
+ENVIRONMENT_SID=
 
-# Voz Calida
-#SERVICE_SID=ZS8ed6db191546b9aac14282790f6eb6a9
-#ENVIRONMENT_SID=ZEd89debfc86b0818b06b5d2242a22a062
 
 #twilio api:serverless:v1:services:environments:list --service-sid $SERVICE_SID
 
