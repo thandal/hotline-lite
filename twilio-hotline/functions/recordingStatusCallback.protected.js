61a54ea4ce32dcc8e0223c17dc4c163cfeedac6e
larsakerson <22251261+larsakerson@users.noreply.github.com>
2026-07-24 12:46:05 -0400

    Create general method for preparing attachments

--- twilio-hotline/functions/recordingStatusCallback.protected.js
+++ twilio-hotline/functions/recordingStatusCallback.protected.js
@@ -1,26 +1,17 @@
 exports.handler = async function (context, event, callback) {
   console.log('/recordingStatusCallback ' + event.RecordingUrl);
   const { langMap, formatE164 } = require(Runtime.getAssets()['/language.js'].path);
-  const axios = require('axios');
-  const fs = require('fs');
-  const tmp_dir = require('os').tmpdir();
-  const recordingResponse = await axios.get(event.RecordingUrl + '.mp3',
-    {
-      auth: {
-        username: context.ACCOUNT_SID,
-        password: context.AUTH_TOKEN,
-      },
-      responseType: 'arraybuffer',
-    },
-  );
+  const { notify, prepareAttachment } = require(Runtime.getAssets()['/notify.js'].path);
+
   const recordingStartTime = new Date(event.RecordingStartTime);
   // Colons are not filesystem/Dropbox-safe, so strip them out of the ISO timestamp.
   const timestamp = recordingStartTime.toISOString().replace(/:/g, '-');
   const attachment_filename = timestamp + '_' + event.callerFrom + '.mp3';
-  const attachment_path = tmp_dir + '/' + attachment_filename;
-  fs.writeFileSync(attachment_path, recordingResponse.data);
-  const { notify } = require(Runtime.getAssets()['/notify.js'].path);
+  const recording_url = event.RecordingUrl + '.mp3';
+  const attachment_path = await prepareAttachment(context, recording_url, attachment_filename);
+
   const languageName = langMap[event.language] ? langMap[event.language].name : event.language;
   await notify(context, 'New voice memo in ' + languageName + ' from ' + formatE164(event.callerFrom), attachment_path);
+  
   return callback(null, 'OK');
 }
