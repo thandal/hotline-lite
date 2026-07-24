f5599a282a57d4de766cdcd890151e9084e9201f
larsakerson <22251261+larsakerson@users.noreply.github.com>
2026-07-24 18:41:25 -0400

    Rename inbound message endpoint
    It's not just for SMS anymore

--- setup_hotline.sh
+++ setup_hotline.sh
@@ -229,7 +229,7 @@ echo "Configuring the phone number callbacks..."
 twilio api:core:incoming-phone-numbers:update \
     --sid $PHONE_NUMBER_SID \
     --voice-url="https://$SERVICE_DOMAIN_BASE-dev.twil.io/hotline" \
-    --sms-url="https://$SERVICE_DOMAIN_BASE-dev.twil.io/inboundSmsMessage" \
+    --sms-url="https://$SERVICE_DOMAIN_BASE-dev.twil.io/inboundMessage" \
     --status-callback="https://$SERVICE_DOMAIN_BASE-dev.twil.io/clearLogs"
 
 echo  # (optional) move to a new line
